6d90f70d788e80b0aa9fa038b9e58ca60f34a61a
juinchuen <juinoh2024@u.northwestern.edu>
2023-10-23 00:55:41 -0500

    cpu prototype

---
+++ lab2/sc_cpu.v
@@ -0,0 +1,12 @@
+module sc_cpu(halt, clk, rst)
+
+    output halt;
+    input clk, rst;
+
+
+
+    InstMem im0 (InstAddr, InstSize, InstOut, clk);
+    DataMem dm0 (DataAddr, DataSize, DataIn, DataOut, DWEN, clk);
+    RegFile rf0 (AddrA, DataOutA, AddrB, DataOutB, AddrW, DataInW, RWEN, clk);
+
+endmodule
